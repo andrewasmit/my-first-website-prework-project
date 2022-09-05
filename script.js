2e2144f2cd76eb572bd484dfc155411a3b3b6b30
andrew <andrewasmit@hotmail.com>
2022-09-05 10:02:03 -0500

    Added a button and styled it accordingly. Added first JS element to button(Alert Message)

---
+++ script.js
@@ -0,0 +1,5 @@
+const clickMessage = document.getElementById("simple-js-message");
+
+clickMessage.addEventListener("click", function(){
+    alert('You clicked the "JavaScript Message" button and now this message appeared!');
+});
